95dcaacfce93de5576d8d64363d9baea74c1fd23
e-nara <enaracraft@mail.com>
2024-03-01 22:23:14 +0000

    add repo

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("loaded");
+
+const myBuzzer = document.getElementById("buzzer");
+console.log(myBuzzer);
+
+
+function playSound() {
+    const audio = new Audio('buzzer.mp3');
+    console.log("ouch");
+    audio.play();
+}
